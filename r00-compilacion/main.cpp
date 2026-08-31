a865154e48ccc91a816c8f8a08309ec9e342ef16
Enrique <kikedurzo@gmail.com>
2026-08-31 17:26:52 -0600

    feat: agregar gitignore y sesion r00-compilacion

---
+++ r00-compilacion/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "suma.h"
+using namespace std;
+
+int main(){
+    int resultado = suma(120,40);
+    cout<< resultado << endl;
+    return 0;
+}
---
+++ r00-compilacion/suma.cpp
@@ -0,0 +1,5 @@
+#include "suma.h"
+
+int suma(int a, int b){
+    return a + b;
+}
