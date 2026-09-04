4ca50ed9611fa4e096e9e0fa9ad699e9c932183d
Enrique <kikedurzo@gmail.com>
2026-09-03 18:02:20 -0600

    R11: separacion de Punto en punto.h, punto.cpp y main.cpp con Makefile

---
+++ r11-multiarchivo/main.cpp
@@ -0,0 +1,9 @@
+#include "punto.h"
+
+Punto a;          // Punto()
+Punto b(5);       // Punto(5)
+Punto c(1, 2);    // Punto(1,2)
+
+int main() {
+    return 0;
+}
---
+++ r11-multiarchivo/punto.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Punto {
+    int x, y;
+public:
+    Punto();
+    Punto(int a);
+    Punto(int a, int b);
+};
+
